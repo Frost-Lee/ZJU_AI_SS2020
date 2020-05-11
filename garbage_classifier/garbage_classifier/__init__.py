e91cae41bcc59c303ef9e962dd020c2c5a7afe40
Frost Lee <canchen.lee@gmail.com>
2020-05-11 01:06:25 -0400

    full training ipynb.

---
+++ garbage_classifier/garbage_classifier/__init__.py
@@ -0,0 +1,3 @@
+from . import data_feed
+from . import model
+from . import utils
--- garbage_classifier/garbage_classifier/data_feed.py
+++ garbage_classifier/garbage_classifier/data_feed.py
@@ -2,7 +2,7 @@ import numpy as np
 import imgaug as ia
 from tensorflow import keras
 
-def data_feed(path, sample_path, input_size, batch_size=4, validation_split=0.1):
+def data_feed(path, input_size, sample_path=None, batch_size=4, validation_split=0.1):
     def image_preprocessing(images):
         sometimes = lambda x: ia.augmenters.Sometimes(0.5, x)
         augmentation_sequence = ia.augmenters.Sequential([
@@ -11,8 +11,8 @@ def data_feed(path, sample_path, input_size, batch_size=4, validation_split=0.1)
         ])
         return augmentation_sequence(images=images)
     data_generator = keras.preprocessing.image.ImageDataGenerator(
-        featurewise_center=True,
-        featurewise_std_normalization=True,
+        featurewise_center=sample_path is not None,
+        featurewise_std_normalization=sample_path is not None,
         rotation_range=180,
         width_shift_range=0.2,
         height_shift_range=0.2,
@@ -22,7 +22,8 @@ def data_feed(path, sample_path, input_size, batch_size=4, validation_split=0.1)
         preprocessing_function=image_preprocessing,
         validation_split=validation_split
     )
-    data_generator.fit(np.load(sample_path))
+    if sample_path is not None:
+        data_generator.fit(np.load(sample_path))
     flow_args = {
         'directory': path,
         'target_size': input_size,
